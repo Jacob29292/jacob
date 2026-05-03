15275392369c4ff514548e2c5e990092efd0af32
Jacob29292 <bergeronjacob292@gmail.com>
2026-05-03 15:08:59 -0400

    fix: add safe boot scene and force canvas renderer

--- pocketkaiju/src/phaser/game.ts
+++ pocketkaiju/src/phaser/game.ts
@@ -1,11 +1,12 @@
 import Phaser from 'phaser';
 import { AttackTestScene } from './scenes/AttackTestScene';
+import { BootScene } from './scenes/BootScene';
 import { CollectionScene } from './scenes/CollectionScene';
 import { DefenseTestScene } from './scenes/DefenseTestScene';
 import { MenuScene } from './scenes/MenuScene';
 
 export const launchGame = (parent: string): Phaser.Game => new Phaser.Game({
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   width: 1200,
   height: 720,
   parent,
@@ -16,5 +17,5 @@ export const launchGame = (parent: string): Phaser.Game => new Phaser.Game({
     width: 1200,
     height: 720
   },
-  scene: [MenuScene, AttackTestScene, DefenseTestScene, CollectionScene]
+  scene: [BootScene, MenuScene, AttackTestScene, DefenseTestScene, CollectionScene]
 });
